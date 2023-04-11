bfa1f61e76a9b3ff4588cbf5c4dd283851b1ff8f
Rodney <rodneyruan@gmail.com>
2023-04-11 14:56:40 +0800

    Update get_file_from_FTP_server.sh

--- ShellScripts/get_file_from_FTP_server.sh
+++ ShellScripts/get_file_from_FTP_server.sh
@@ -1,10 +1,32 @@
-FTP_SERVER=10.88.70.151
-ftp -n << _EOF_
-open $FTP_SERVER
-user username password
-bin
-get $1 
-bye
+#!/bin/bash
+# A shell script to get a file from an FTP server
+# Usage: ./get_file.sh ip username password filename
 
-_EOF_
-echo "FTP download $1 is complete!"
+# Check if the number of arguments is 4
+if [ $# -ne 4 ]; then
+  echo "Invalid number of arguments."
+  echo "Usage: ./get_file.sh ip username password filename"
+  exit 1
+fi
+
+# Assign the arguments to variables
+ip=$1
+username=$2
+password=$3
+filename=$4
+
+# Use ftp command to connect to the server and get the file
+ftp -n $ip <<END_SCRIPT
+user $username $password
+binary
+get $filename
+quit
+END_SCRIPT
+
+# Check if the file was downloaded successfully
+if [ -f $filename ]; then
+  echo "File downloaded successfully."
+else
+  echo "File download failed."
+  exit 2
+fi
